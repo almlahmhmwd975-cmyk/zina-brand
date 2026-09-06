adcbd65dcc34b8e2c71b3b637442e337a5c581f5
mahmoud <almlahmhmwd975@gmail.com>
2026-09-06 23:42:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ document.addEventListener("DOMContentLoaded", () => {
             if (email === "zina@brand.com" && password === "zina2026") {
                 alert("مرحباً بكِ يا زينة! جاري توجيهك إلى لوحة التحكم...");
                 loginModal.style.display = "none";
-                window.location.href = "/admin/";
+               window.location.href = "/admin/index.html";
+
             } else {
                 alert("عذراً، البريد الإلكتروني أو كلمة السر غير صحيحة!");
             }
